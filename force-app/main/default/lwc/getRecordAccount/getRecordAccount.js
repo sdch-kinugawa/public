919784e9520257a5d0cc5c291d8702b4f2be6453
sdch-kinugawa <kinugawa@sodech.com>
2025-03-13 18:00:28 +0900

    修正しました。

--- force-app/main/default/lwc/getRecordAccount/getRecordAccount.js
+++ force-app/main/default/lwc/getRecordAccount/getRecordAccount.js
@@ -9,7 +9,7 @@ export default class getRecordAccount extends LightningElement {
     @api recordId;
 
     // 引数1:実行したいメソッド名
-    // 引数2:{ recordId: 取得したいSFID, fields: 取得したい項目}
+    // 引数2:{ recordId: 取得したいSFID, fields: 取得したい項目}　呼び出したいメソッドによって、引数の変数は異なります。
     @wire(getRecord, { recordId: '$recordId', fields: FIELDS})
     acc; // wireで実行された戻り値を格納する変数
 
