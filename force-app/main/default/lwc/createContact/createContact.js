1bb468f31385483c5d56fb8d31bcf4e5c1e9ce15
sdch-kinugawa <kinugawa@sodech.com>
2025-03-14 14:20:11 +0900

    新規作成

---
+++ force-app/main/default/lwc/createContact/createContact.js
@@ -0,0 +1,102 @@
+import { LightningElement } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi';
+import CONTACT_OBJECT from "@salesforce/schema/Contact";
+// 各種項目
+import LASTNAME_FIELD from "@salesforce/schema/Contact.LastName";
+import FIRSTNAME_FIELD from "@salesforce/schema/Contact.FirstName";
+import EMAIL_FIELD from "@salesforce/schema/Contact.Email";
+import ACCOUNTID_FIELD from "@salesforce/schema/Contact.AccountId";
+// メッセージ表示用
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+export default class CreateContact extends LightningElement {
+
+    // 変数宣言
+    LastName;
+    FirstName;
+    Email;
+    AccountId;
+
+    async handleCreate() {
+        const fields = {};
+        // 画面からの入力値を取得
+        fields[LASTNAME_FIELD.fieldApiName] = this.template.querySelector("[data-field='LastName']",).value;
+        fields[FIRSTNAME_FIELD.fieldApiName] = this.template.querySelector("[data-field='FirstName']",).value;
+        fields[EMAIL_FIELD.fieldApiName] = this.template.querySelector("[data-field='Email']",).value;
+        fields[ACCOUNTID_FIELD.fieldApiName] = this.template.querySelector("[data-field='AccountId']",).value;
+    
+        const recordInput = { apiName: CONTACT_OBJECT.objectApiName, fields };
+        try {
+          // createRecord実施
+          const contact = await createRecord(recordInput)
+          if(contact){
+            // 正常終了
+            this.showToast("成功", '作成完了しました。', 'success');
+            // 画面入力値初期化
+            this.initVal();
+          }
+
+        } catch (error) {
+            // 電話番号に必須の入力規則を入れてエラーを確認してみましょう。
+            this.showToast("失敗", error.body.output.errors[0].message, 'error');
+        }
+    }
+
+    // 画面入力値初期化
+    initVal() {
+
+        this.LastName= '';
+        this.FirstName= '';
+        this.Email = '';
+        this.AccountId = '';
+
+    }
+
+    // ================================
+    // 参照項目：取引先の検索条件設定
+    // ================================
+    filterAcc = {
+        criteria: [
+            {
+                fieldPath: 'Website',
+                operator: 'ne',
+                value: null,
+            },
+            {
+                fieldPath: 'Phone',
+                operator: 'ne',
+                value: null,
+            },
+            {
+                fieldPath: 'Type',
+                operator: 'ne',
+                value: 'Partner',
+            }
+        ],
+        filterLogic: '(1 OR 2) AND 3',
+    };
+    // ================================
+    // 参照項目：表示する情報設定
+    // ================================
+    displayInfoAcc = {
+        primaryField: 'Name',
+        additionalFields: ['Phone'],
+    };
+    // ================================
+    // 参照項目：表示する情報設定
+    // ================================
+    matchingInfoAcc = {
+        primaryField: { fieldPath: 'Name', mode: 'startsWith' },
+        additionalFields: [{ fieldPath: 'Phone' }],
+    };
+
+    // メッセージ生成
+    showToast(title, message, variant) {
+        const event = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+        });
+        this.dispatchEvent(event);
+    }
+
+}
