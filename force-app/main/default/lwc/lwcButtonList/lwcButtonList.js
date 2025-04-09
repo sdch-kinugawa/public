1a39470fd53d748dd7ee3cfcc5ec360e31ce748f
sdch-kinugawa <kinugawa@sodech.com>
2025-04-09 18:04:33 +0900

    コメント修正

--- force-app/main/default/lwc/lwcButtonList/lwcButtonList.js
+++ force-app/main/default/lwc/lwcButtonList/lwcButtonList.js
@@ -3,26 +3,18 @@ import { NavigationMixin } from 'lightning/navigation';
 
 export default class LwcButtonList extends NavigationMixin(LightningElement) {
 
-    @api recordId; //商談Id
+    @api recordId; // 請求Id
 
     connectedCallback() {
         console.log('this.recordId=='+this.recordId);
     }
-
-    // handleOppCreateOnClick(){
-    //     const baseUrl = window.location.origin;
-    //     let pageUrl = `${baseUrl}/lightning/n/CreateOpp?c__oppRecordId=${this.recordId}`;
-    //     console.log('pageUrl == ' + pageUrl);
-    //     // 見積作成画面に遷移
-    //     window.open(pageUrl, '_self');
-    // }
 
     handleBillCreateOnClick(){
         const baseUrl = window.location.origin;
         // CreateBillは作成したタブのAPI名を指定
         let pageUrl = `${baseUrl}/lightning/n/CreateBill?c__billRecordId=${this.recordId}`;
         console.log('pageUrl == ' + pageUrl);
-        // 請求作成画面に遷移
+        // CreateBillタブへ遷移
         window.open(pageUrl, '_self');
     }
 
